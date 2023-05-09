4568ac94d2a974937a47bc25dd285969a4d0936a
ariel1985 <arielrotem85@gmail.com>
2023-05-09 23:19:17 +0300

    bugfix

--- src/components/Component/AboutSpotit.tsx
+++ src/components/Component/AboutSpotit.tsx
@@ -66,9 +66,9 @@ function AboutSpotit() {
             height="315"
             src="https://www.youtube.com/embed/BJBJPWmZENA"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </Accordion.Content>
       </Accordion>
