ca40af85f9ffaec7f8cd3e9e5900af32c80456ad
Yuval Dikerman <3dyuval@gmail.com>
2023-05-10 19:23:10 +0300

    Jimp will not compile Fixes #4

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import EnvironmentPlugin from 'vite-plugin-environment'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+      emptyOutDir: true,
     assetsInlineLimit: 4096 * 300
   },
   plugins: [react()],
